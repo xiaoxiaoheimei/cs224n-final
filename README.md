875465f73b271a3d8ac05ea2b85c400477984e99
Lisa Song <lisasong@stanford.edu>
2019-02-26 23:03:21 -0800

    instantiate BERT embeddings and readme wording

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# cs224n-final
-final project of cs224n
+
+### In Construction 
+
+# CS224n-final with BERT pre-trained embeddings and BiDAF attention flow
+
+## 0. Introduction
+
+This repository is for Liu and Lijing's final project of cs224n for default SQuAD 2.0 task based on [PyTorch reimplementation](https://github.com/huggingface/pytorch-pretrained-BERT) of [Google's TensorFlow repository for the BERT model](https://github.com/google-research/bert) with the paper: [BERT: Pre-training of Deep Bidirectional Transformers for Language Understanding](https://arxiv.org/abs/1810.04805).
---
+++ word_embeddings.py
@@ -0,0 +1,22 @@
+"""Following code inspired by [BERT FineTuning with Cloud TPU: Sentence and Sentence-Pair Classification Tasks]
+https://colab.research.google.com/github/tensorflow/tpu/blob/master/tools/colab/bert_finetuning_with_cloud_tpus.ipynb#scrollTo=tYkaAlJNfhul"""
+
+import os
+import sys
+
+!test -d pytorch-pretrained-BERT || git clone https://github.com/huggingface/pytorch-pretrained-BERT
+if not 'pytorch-pretrained-BERT' in sys.path:
+    sys.path += ['pytorch-pretrained-BERT']
+
+cwd = os.getcwd()
+DataPath = cwd + "\\bert\\"
+
+# Available pretrained model checkpoints:
+#   uncased_L-12_H-768_A-12: uncased BERT base model
+#   uncased_L-24_H-1024_A-16: uncased BERT large model
+#   cased_L-12_H-768_A-12: cased BERT large model
+BERT_MODEL = 'uncased_L-12_H-768_A-12' #@param {type:"string"}
+BERT_PRETRAINED_DIR = DataPath + BERT_MODEL
+print('***** BERT pretrained directory: {} *****'.format(BERT_PRETRAINED_DIR))
+!gsutil ls $BERT_PRETRAINED_DIR
+
