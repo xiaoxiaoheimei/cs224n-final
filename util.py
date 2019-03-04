75d0c06c67f776f4c349d7a878f442c3b9bf12e6
xiaoxiaoheimei <ylustc@gmail.com>
2019-03-04 17:06:01 +0800

    Add BertSQuAD class to read in data set in Pytorch tensor form.

--- util.py
+++ util.py
@@ -86,6 +86,58 @@ class SQuAD(data.Dataset):
     def __len__(self):
         return len(self.valid_idxs)
 
+class BertSQuAD(data.Dataset):
+    """Stanford Question Answering Dataset (SQuAD) with Bert Token.
+
+    Each item in the dataset is a tuple with the following entries (in order):
+        - context_idxs: Bert ID of the words in the context.
+            Shape (context_len,).
+        - context_char_idxs: Characer IDs (without '##') of each Bert token.
+            Shape (context_len, max_word_len).
+        - question_idxs: Bert ID of the words in the question.
+            Shape (question_len,).
+        - question_char_idxs: ID of the characters of Bert tokens in the question.
+            Shape (question_len, max_word_len).
+        - y1: Index of word in the context where the answer begins.
+            -1 if no answer.
+        - y2: Index of word in the context where the answer ends.
+            -1 if no answer.
+        - id: ID of the example.
+
+    Args:
+        data_path (str): Path to .npz file containing pre-processed dataset.
+        use_v2 (bool): Whether to use SQuAD 2.0 questions. Otherwise only use SQuAD 1.1.
+    """
+    def __init__(self, data_path, use_v2=True):
+        super(BertSQuAD, self).__init__()
+
+        dataset = np.load(data_path)
+        self.context_idxs = torch.from_numpy(dataset['context_idxs']).long()
+        self.context_char_idxs = torch.from_numpy(dataset['context_char_idxs']).long()
+        self.question_idxs = torch.from_numpy(dataset['ques_idxs']).long()
+        self.question_char_idxs = torch.from_numpy(dataset['ques_char_idxs']).long()
+        self.y1s = torch.from_numpy(dataset['y1s']).long()
+        self.y2s = torch.from_numpy(dataset['y2s']).long()
+
+        # SQuAD 1.1: Ignore no-answer examples
+        self.ids = torch.from_numpy(dataset['ids']).long()
+        self.valid_idxs = [idx for idx in range(len(self.ids))
+                           if use_v2 or self.y1s[idx].item() >= 0]
+
+    def __getitem__(self, idx):
+        idx = self.valid_idxs[idx]
+        example = (self.context_idxs[idx],
+                   self.context_char_idxs[idx],
+                   self.question_idxs[idx],
+                   self.question_char_idxs[idx],
+                   self.y1s[idx],
+                   self.y2s[idx],
+                   self.ids[idx])
+
+        return example
+
+    def __len__(self):
+        return len(self.valid_idxs)
 
 def collate_fn(examples):
     """Create batch tensors from a list of individual examples returned
